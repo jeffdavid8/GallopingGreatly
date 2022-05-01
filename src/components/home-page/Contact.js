2b5a3f39c62885055d2a877dd3b55dc7205f01fd
Jeff David <jeff.david@mediabrain.net>
2022-05-01 07:51:45 -0400

    Adding footer buttons and styling

--- src/components/home-page/Contact.js
+++ src/components/home-page/Contact.js
@@ -34,7 +34,7 @@ const Contact = () => {
           <div className="hr"></div>
         </div>
         <div className="row">
-          <div className="col">
+          <div className="col s12">
             <p className="">** Appointments are by request, and availability is subject to change.</p>
             <p>Please contact us to book an appointment.&nbsp;</p>
           </div>
@@ -52,10 +52,6 @@ const Contact = () => {
             <div className="input-field _2nVk2 _65cjg">
               <input type="email" name="email" id="input_comp-jsivube7" className="_1SOvY has-custom-focus" placeholder="" required="" maxLength="250" />
               <label htmlFor="input_comp-jsivube7" className="aHD7c">Email</label>
-            </div>
-            <div className="input-field _2nVk2">
-              <input type="text" name="subject" id="input_comp-jsivzldo" className="_1SOvY has-custom-focus" placeholder="" />
-              <label htmlFor="input_comp-jsivzldo" className="aHD7c">Subject</label>
             </div>
             <div className="_1mQNr">
               <label htmlFor="textarea_comp-jsivubec" className="_20uhs">Message</label>
@@ -80,14 +76,32 @@ const Contact = () => {
         <div className="row">
           <div className="col s12 m6">
             <p className="font_8"><span>Galloping Greatly</span></p>
-            <p className="font_8"><span>9164 E 825 S,</span></p>
+            <a
+              className="btn btn-round blue lighten-3"
+              title="Navigate"
+              target="_blank"
+              href="https://www.google.com/maps/dir//9164+E+825+S,+Upland,+IN+46989/@40.4321404,-85.5011047,17z"
+            >
+              <i className="material-icons">navigation</i> 
+            </a>
+            <p className="font_8">
+              <span>9164 E 825 S,</span></p>
 
             <p className="font_8"><span>Upland, IN 46989</span></p>
 
             <p className="font_8"><span><a data-auto-recognition="true" href="mailto:gallopinggreatly@gmail.com">gallopinggreatly@gmail.com</a></span></p>
 
-            <p className="font_8">
-            <span><span><a href="https://www.google.com/search?client=safari&amp;rls=en&amp;q=rope+this+ranch+upland&amp;ie=UTF-8&amp;oe=UTF-8#" target="_blank" rel="noreferrer noopener">(765) 506-6462</a></span></span></p>
+            <a
+              className="btn btn-round green"
+              title="Call"
+              target="_blank"
+              href="tel: 7655066462"
+            >
+              <i className="material-icons">call</i> 
+            </a>
+
+            <p className="font_8">(765) 506-6462
+            <span><a href="tel: 7655066462" target="_blank" rel="noreferrer noopener"></a></span></p>
 
             <p className="font_8">&nbsp;</p>
           </div>
